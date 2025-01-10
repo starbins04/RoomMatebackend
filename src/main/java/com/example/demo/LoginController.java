187113e3f51ec36a5d4ba471ca61082ed31c8510
starbins04 <starbins004@gmail.com>
2025-01-11 06:02:42 +0900

    ~~~~수!정

--- src/main/java/com/example/demo/LoginController.java
+++ src/main/java/com/example/demo/LoginController.java
@@ -11,12 +11,14 @@ public class LoginController {
     private List<Login> loginList = new ArrayList<>();
 
     // 회원가입 처리
-    @PostMapping("/login/signup")
-    public String addLogin(@RequestBody AddLoginRequestDto requestDto) {
+    @PostMapping("/login")
+    public Integer addLogin(@RequestBody AddLoginRequestDto requestDto) {
         // 이미 등록된 ID가 있는지 확인
         for (Login login : loginList) {
             if (login.getId().equals(requestDto.getId())) {
-                return "ID already exists. Please choose a different ID.";
+                if (login.getPassword().equals(requestDto.getPassword())) {
+                    return 1; // 로그인 성공
+                }
             }
         }
 
@@ -24,24 +26,10 @@ public class LoginController {
         Login login = new Login(requestDto.getId(), requestDto.getPassword());
         loginList.add(login);
         System.out.println(loginList + " 저장됨");
-        return "Registration successful!";
+        return 0;
     }
 
-    // 로그인 처리
-    @PostMapping("/login/authenticate")
-    public Integer authenticateLogin(@RequestBody AddLoginRequestDto requestDto) {
-        // ID와 비밀번호 검증
-        for (Login login : loginList) {
-            if (login.getId().equals(requestDto.getId())) {
-                if (login.getPassword().equals(requestDto.getPassword())) {
-                    return 1; // 로그인 성공
-                } else {
-                    return 0; // 비밀번호 오류
-                }
-            }
-        }
-        return 0; // ID 없음
-    }
+
 
     // 로그인 목록 조회 (디버깅용)
     @GetMapping("/login")
